277855c4b2aa593349d1ab96d1e13c15c731a3fc
internet-unable <k_moiseenko@icloud.com>
2025-02-06 01:39:17 +0200

    Update App
    - edit dummy data;

--- 01-react_recipe-page_newbie/src/App.jsx
+++ 01-react_recipe-page_newbie/src/App.jsx
@@ -1,3 +1,4 @@
+// import { useState, useEffect } from 'react';
 import Panel from './components/UI/Panel/Panel';
 
 const DUMMY_DATA = [
@@ -31,27 +32,27 @@ const DUMMY_DATA = [
             items: [
                 {
                     key: "Beat the eggs",
-                    value: "Add later"
+                    value: "In a bowl, beat the eggs with a pinch of salt and pepper until they are well mixed. You can add a tablespoon of water or milk for a fluffier texture."
                 },
                 {
                     key: "Heat the pan",
-                    value: "Add later"
+                    value: "Place a non-stick frying pan over medium heat and add butter or oil."
                 },
                 {
-                    key: "Cook the omlete",
-                    value: "Add later"
+                    key: "Cook the omelette",
+                    value: "Once the butter is melted and bubbling, pour in the eggs. Tilt the pan to ensure the eggs evenly coat the surface."
                 },
                 {
                     key: "Add fillings (optional)",
-                    value: "Add later"
+                    value: "When the eggs begin to set at the edges but are still slightly runny in the middle, sprinkle your chosen fillings over one half of the omelette."
                 },
                 {
                     key: "Fold and serve",
-                    value: "Add later"
+                    value: "As the omelette continues to cook, carefully lift one edge and fold it over the fillings. Let it cook for another minute, then slide it onto a plate."
                 },
                 {
                     key: "Enjoy",
-                    value: "Add later"
+                    value: "Serve hot, with additional salt and pepper if needed."
                 }
             ]
         },
@@ -81,8 +82,28 @@ const DUMMY_DATA = [
 ]
 
 export default function App() {
+    // const [counter, setCounter] = useState(0);
+
+    // useEffect(() => {
+    //     console.log("Effect");
+    // }, []);
+
+    // function handleCounterIncrease() {
+    //     setCounter(counter + 1);
+    // }
+
+    // function handleCounterDecrease() {
+    //     setCounter(counter - 1);
+    // }
+
     return (
         <main>
+            {/* <div>
+                <button onClick={handleCounterIncrease}>Increase counter</button>
+                <span>{counter}</span>
+                <button onClick={handleCounterDecrease}>Decrease counter</button>
+            </div> */}
+
             <Panel data={DUMMY_DATA[0]} />
         </main>
     )
