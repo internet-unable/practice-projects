5bb974c7ba3ced3720b5ec7526ddae956a0fc05e
internet-unable <k_moiseenko@icloud.com>
2025-02-09 01:09:59 +0200

    Create Recipe page

---
+++ 01-react_recipe-page_newbie/src/components/Pages/Recipe/Recipe.jsx
@@ -0,0 +1,40 @@
+import Card, { cardSizes } from "../../UI/Card/Card";
+import Heading from "../../UI/Heading/Heading";
+import List from "../../UI/List/List";
+import Divider from "../../UI/Divider/Divider";
+import Table from "../../UI/Table/Table";
+
+import styles from "./Recipe.module.css";
+
+const listPaddingBottom = {
+    paddingBottom: "31px"
+}
+
+export default function Recipe({ data }) {
+    return (
+        <section className={styles["recipe-page"]}>
+            <Card cardSize={cardSizes.L}>
+                <img src={data.image} style={{ borderRadius: "15px" }} />
+                <Heading tag="h1">{data.title}</Heading>
+                <p>{data.description}</p>
+
+                <Card className="bg-rose--50" cardSize={cardSizes.S}>
+                    <Heading tag="h3" className="font-outfit--bold color-rose--800">{data.preparation.title}</Heading>
+                    <List data={data.preparation.items} listStyle="list-style--bg-rose-800" />
+                </Card>
+
+                <Heading tag="h2" className="color-brown--800">{data.ingredients.title}</Heading>
+                <List data={data.ingredients.items} listStyle="list-style--square list-style--bg-brown-800" style={listPaddingBottom} />
+                <Divider />
+
+                <Heading tag="h2" className="color-brown--800">{data.instructions.title}</Heading>
+                <List data={data.instructions.items} listStyle="list-style--decimal list-style--c-brown-800" style={listPaddingBottom} />
+                <Divider />
+
+                <Heading tag="h2" className="color-brown--800">{data.nutrition.title}</Heading>
+                <p style={{ marginBottom: "26px" }}>{data.nutrition.description}</p>
+                <Table data={data.nutrition.items} />
+            </Card>
+        </section>
+    );
+}
