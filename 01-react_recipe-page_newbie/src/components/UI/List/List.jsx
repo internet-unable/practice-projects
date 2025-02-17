4d6a63d75b0967a50b950a7e842de76a2cc003af
internet-unable <k_moiseenko@icloud.com>
2025-02-17 21:57:06 +0200

    Update List
    - change usage of classListToModules to pre-defined listStyles object;

--- 01-react_recipe-page_newbie/src/components/UI/List/List.jsx
+++ 01-react_recipe-page_newbie/src/components/UI/List/List.jsx
@@ -1,9 +1,14 @@
-import { classListToModules } from "../../../utils";
 import styles from "./List.module.css";
 
-export default function List({ data, listStyle, ...rest }) {
+export const listStyles = {
+    defaultBgRose800: `${styles["list-style--bg-rose-800"]}`,
+    squareBgBrown800: `${styles["list-style--square"]} ${styles["list-style--bg-brown-800"]}`,
+    decimalCBrown800: `${styles["list-style--decimal"]} ${styles["list-style--c-brown-800"]}`,
+}
+
+export default function List({ className = "", data, listStyle, ...rest }) {
     return (
-        <ul className={listStyle && classListToModules(styles.list, listStyle, styles)} {...rest}>
+        <ul className={`${className} ${listStyle}`} {...rest}>
             {data.map(item => (
                 <li className={styles["list-item"]} key={item.id}>
                     <div className={styles["list-item__content"]}>
