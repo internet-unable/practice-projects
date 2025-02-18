8624ad7bec60d9bc3b08c57f03fc3c01d97ddc69
internet-unable <k_moiseenko@icloud.com>
2025-02-18 18:02:00 +0200

    Create HeadingWithIcon

--- 02-react_ticket-generator_junior/src/components/UI/HeadingWithIcon/HeadingWithIcon.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/HeadingWithIcon/HeadingWithIcon.jsx
@@ -1,11 +1,11 @@
 import styles from "./HeadingWithIcon.module.css";
 
 export const iconStyles = {
-    sizeS_posLeft: "",
+    sizeS_posLeft: `${styles["icon-size--s"]} ${styles["icon-pos--left"]}`,
 }
 
 export default function HeadingWithIcon({ iconStyle, children }) {
     return (
-        <div className={styles.iconStyle}>{children}</div>
+        <div className={`${styles["heading-with-icon"]} ${iconStyle}`}>{children}</div>
     );
 }
