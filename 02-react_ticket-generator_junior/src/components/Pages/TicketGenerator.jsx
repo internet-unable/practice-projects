7606d96c07aca1feb9056e1989f4c97c4f8bc4ce
internet-unable <k_moiseenko@icloud.com>
2025-02-19 18:07:13 +0200

    Update TicketGenerator
    - apply changes;

--- 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
+++ 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
@@ -1,41 +1,35 @@
-import Header from "../../Sections/Header/Header";
+import Header from "../Sections/Header/Header";
 import Heading from "../UI/Heading/Heading";
+
+import Input from "../UI/Form/Input/Input";
+import Button from "../UI/Button/Button";
 
 export default function TicketGenerator() {
     return (
         <>
             <Header />
 
-            <section>
-                <Heading tag="h1">Your Journey to Coding Conf 2025 Starts Here!</Heading>
-                <p>Secure your spot at next year`s biggest coding conference.</p>
-            </section>
+            <main className="text--center" style={{ paddingTop: "60px" }}>
+                <section style={{ marginBottom: "50px" }}>
+                    <Heading tag="h1" className="margin--center test" style={{ maxWidth: "800px" }}>Your Journey to Coding Conf 2025 Starts Here!</Heading>
+                    <p>Secure your spot at next year`s biggest coding conference.</p>
+                </section>
 
-            <section>
-                <form>
-                    <div>
-                        <label htmlFor="avatar">Upload avatar</label>
-                        <input type="file" id="avatar" />
-                    </div>
+                <section>
+                    <form>
+                        <div>
+                            <label htmlFor="avatar">Upload avatar</label>
+                            <input type="file" id="avatar" />
+                        </div>
 
-                    <div>
-                        <label htmlFor="full-name">Name</label>
-                        <input type="text" id="full-name" />
-                    </div>
-
-                    <div>
-                        <label htmlFor="email">Email address</label>
-                        <input type="email" id="email" placeholder="example@email.com" />
-                    </div>
-                    
-                    <div>
-                        <label htmlFor="github-username">Github Username</label>
-                        <input type="text" id="github-username" placeholder="@yourusername" />
-                    </div>
-
-                    <button>Generate My Ticket</button>
-                </form>
-            </section>
+                        <Input label="Full name" type="text" name="full-name" />
+                        <Input label="Email address" type="email" name="email" placeholder="example@email.com" />
+                        <Input label="Github Username" type="text" name="github-username" placeholder="@yourusername" />
+                        
+                        <Button>Generate My Ticket</Button>
+                    </form>
+                </section>
+            </main>
         </>
     );
 }
