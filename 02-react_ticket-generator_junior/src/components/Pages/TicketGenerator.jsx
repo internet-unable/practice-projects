b46a982a6896aaa7b44fcb269978d697b02ccbb6
internet-unable <k_moiseenko@icloud.com>
2025-02-20 18:56:33 +0200

    Update TicketGenerator
    - apply changes;

--- 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
+++ 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
@@ -9,23 +9,31 @@ export default function TicketGenerator() {
         <>
             <Header />
 
-            <main className="text--center" style={{ paddingTop: "60px" }}>
-                <section style={{ marginBottom: "50px" }}>
+            <main style={{ paddingTop: "60px" }}>
+                <section className="text--center" style={{ marginBottom: "50px" }}>
                     <Heading tag="h1" className="margin--center test" style={{ maxWidth: "800px" }}>Your Journey to Coding Conf 2025 Starts Here!</Heading>
                     <p>Secure your spot at next year`s biggest coding conference.</p>
                 </section>
 
                 <section>
-                    <form>
-                        <div>
-                            <label htmlFor="avatar">Upload avatar</label>
-                            <input type="file" id="avatar" />
+                    <form className="margin--center" style={{ maxWidth: "460px" }}>
+                        <div style={{ marginBottom: "24px" }}>
+                            <div>
+                                <label htmlFor="avatar">Upload avatar</label>
+                                <input type="file" id="avatar" />
+                            </div>
+
+                            <Input label="Full Name" type="text" name="full-name" />
+                            <Input
+                                label="Email Address"
+                                type="email"
+                                name="email"
+                                placeholder="example@email.com"
+                                message="Please enter a valid email address."
+                            />
+                            <Input label="GitHub Username" type="text" name="github-username" placeholder="@yourusername" />
                         </div>
 
-                        <Input label="Full name" type="text" name="full-name" />
-                        <Input label="Email address" type="email" name="email" placeholder="example@email.com" />
-                        <Input label="Github Username" type="text" name="github-username" placeholder="@yourusername" />
-                        
                         <Button>Generate My Ticket</Button>
                     </form>
                 </section>
