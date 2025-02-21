829d6cc6256c210f9b2c32d4c64777e41c02456e
internet-unable <k_moiseenko@icloud.com>
2025-02-21 20:04:49 +0200

    Update TicketGenerator
    - apply changes;

--- 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
+++ 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
@@ -1,6 +1,6 @@
 import Header from "../Sections/Header/Header";
 import Heading from "../UI/Heading/Heading";
-
+import Upload from "../UI/Form/Upload/Upload";
 import Input from "../UI/Form/Input/Input";
 import Button from "../UI/Button/Button";
 
@@ -10,31 +10,50 @@ export default function TicketGenerator() {
             <Header />
 
             <main style={{ paddingTop: "60px" }}>
-                <section className="text--center" style={{ marginBottom: "50px" }}>
+                <section className="text--center" style={{ marginBottom: "51px" }}>
                     <Heading tag="h1" className="margin--center test" style={{ maxWidth: "800px" }}>Your Journey to Coding Conf 2025 Starts Here!</Heading>
                     <p>Secure your spot at next year`s biggest coding conference.</p>
                 </section>
 
                 <section>
                     <form className="margin--center" style={{ maxWidth: "460px" }}>
-                        <div style={{ marginBottom: "24px" }}>
-                            <div>
-                                <label htmlFor="avatar">Upload avatar</label>
-                                <input type="file" id="avatar" />
-                            </div>
+                        <Upload
+                            className="form-elem"
+                            label="Upload Avatar"
+                            name="avatar"
+                            message="Upload your photo (JPG or PNG, max size: 500KB)."
+                        />
 
-                            <Input label="Full Name" type="text" name="full-name" />
-                            <Input
-                                label="Email Address"
-                                type="email"
-                                name="email"
-                                placeholder="example@email.com"
-                                message="Please enter a valid email address."
-                            />
-                            <Input label="GitHub Username" type="text" name="github-username" placeholder="@yourusername" />
-                        </div>
+                        <Input
+                            className="form-elem"
+                            label="Full Name"
+                            type="text"
+                            name="full-name"
+                        />
 
-                        <Button>Generate My Ticket</Button>
+                        <Input
+                            className="form-elem"
+                            label="Email Address"
+                            type="email"
+                            name="email"
+                            placeholder="example@email.com"
+                            message="Please enter a valid email address."
+                        />
+
+                        <Input
+                            className="form-elem"
+                            label="GitHub Username"
+                            type="text"
+                            name="github-username"
+                            placeholder="@yourusername"
+                        />
+
+                        <Button
+                            className="form-elem"
+                            style={{ marginTop: "24px" }}
+                        >
+                            Generate My Ticket
+                        </Button>
                     </form>
                 </section>
             </main>
