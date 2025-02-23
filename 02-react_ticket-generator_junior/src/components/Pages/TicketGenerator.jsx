e84e93d53eb5d69c2a9957b07d94d091abae614f
internet-unable <k_moiseenko@icloud.com>
2025-02-23 18:18:11 +0200

    Update TicketGewnerator
    - apply all new chagnes;
    - add state and handlers, to switch content;

--- 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
+++ 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
@@ -1,60 +1,46 @@
+import { useState } from "react";
+
 import Header from "../Sections/Header/Header";
 import Heading from "../UI/Heading/Heading";
-import Upload from "../UI/Form/Upload/Upload";
-import Input from "../UI/Form/Input/Input";
-import Button from "../UI/Button/Button";
+import Form from "../Sections/Form/Form";
 
 export default function TicketGenerator() {
+    const [name, setName] = useState();
+    const [email, setEmail] = useState();
+    let headingContent = <>Your Journey to Coding Conf<br />2025 Starts Here!</>;
+    let paragraphContent = <>Secure your spot at next year`s biggest coding conference.</>;
+
+    if (name) {
+        headingContent = <>Congrats, <span className="c-gradient--orange-white">{name}!</span><br />Your ticket is ready.</>;
+    }
+
+    if (email) {
+        paragraphContent = <>We've emailed your ticket to<br /><span className="c-orange--700">{email}</span> and will send updates in<br />the run up to the event.</>
+    }
+
+    function handleFormSubmit() {
+        setName("Jonatan Kristof");
+        setEmail("jonatan@email.com");
+    }
+
     return (
         <>
             <Header />
 
-            <main style={{ paddingTop: "60px" }}>
-                <section className="text--center" style={{ marginBottom: "51px" }}>
-                    <Heading tag="h1" className="margin--center test" style={{ maxWidth: "800px" }}>Your Journey to Coding Conf 2025 Starts Here!</Heading>
-                    <p>Secure your spot at next year`s biggest coding conference.</p>
+            <main style={{ paddingTop: name ? "70px" : "60px", paddingBottom: "140px" }}>
+                <section className="text--center" style={{ marginBottom: name ? "80px" : "48px" }}>
+                    <Heading tag="h1" style={{marginBottom: name && "33px"}}>{headingContent}</Heading>
+                    <p>{paragraphContent}</p>
                 </section>
 
-                <section>
-                    <form className="margin--center" style={{ maxWidth: "460px" }}>
-                        <Upload
-                            className="form-elem"
-                            label="Upload Avatar"
-                            name="avatar"
-                            message="Upload your photo (JPG or PNG, max size: 500KB)."
-                        />
+                <section className="margin--center" style={{ width: "460px" }}>
+                    {!name && (
+                        <Form onSubmit={handleFormSubmit} />
+                    )}
 
-                        <Input
-                            className="form-elem"
-                            label="Full Name"
-                            type="text"
-                            name="full-name"
-                        />
-
-                        <Input
-                            className="form-elem"
-                            label="Email Address"
-                            type="email"
-                            name="email"
-                            placeholder="example@email.com"
-                            message="Please enter a valid email address."
-                        />
-
-                        <Input
-                            className="form-elem"
-                            label="GitHub Username"
-                            type="text"
-                            name="github-username"
-                            placeholder="@yourusername"
-                        />
-
-                        <Button
-                            className="form-elem"
-                            style={{ marginTop: "24px" }}
-                        >
-                            Generate My Ticket
-                        </Button>
-                    </form>
+                    {name && (
+                        <div className="margin--center">Ticket</div>
+                    )}
                 </section>
             </main>
         </>
