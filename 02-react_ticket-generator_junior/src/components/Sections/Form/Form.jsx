480d727c2203fdcb4c6d92a34ca2efbb61d2edd8
internet-unable <k_moiseenko@icloud.com>
2025-02-25 19:02:58 +0200

    Update Form
    - add pattern, title, required for inputs;

--- 02-react_ticket-generator_junior/src/components/Sections/Form/Form.jsx
+++ 02-react_ticket-generator_junior/src/components/Sections/Form/Form.jsx
@@ -16,7 +16,10 @@ export default function Form({ onSubmit }) {
                 className={styles["form-item"]}
                 label="Upload Avatar"
                 name="avatar"
-                message="Upload your photo (JPG or PNG, max size: 500KB)."
+                hint="Upload your photo (JPG or PNG, max size: 500KB)."
+                errorMessage="File too large. Please upload a photo under 500KB."
+                accept=".jpg, .jpeg, .png"
+                required
             />
 
             <Input
@@ -24,6 +27,9 @@ export default function Form({ onSubmit }) {
                 label="Full Name"
                 type="text"
                 name="full-name"
+                pattern="[A-Za-z]+"
+                title="Please enter a valid full name. Numbers and special characters are not allowed."
+                required
             />
 
             <Input
@@ -33,6 +39,8 @@ export default function Form({ onSubmit }) {
                 name="email"
                 placeholder="example@email.com"
                 message="Please enter a valid email address."
+                title="Please enter a valid email address."
+                required
             />
 
             <Input
@@ -41,6 +49,9 @@ export default function Form({ onSubmit }) {
                 type="text"
                 name="github-username"
                 placeholder="@yourusername"
+                pattern="^@[A-Za-z0-9_]+$"
+                title="Please enter a valid github username. It should start with @ and contain only letters, numbers, and underscores."
+                required
             />
 
             <Button
