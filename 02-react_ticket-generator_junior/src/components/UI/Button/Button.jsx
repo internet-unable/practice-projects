58443c1bbf16657ccba048de2e37dee22bf04e60
internet-unable <k_moiseenko@icloud.com>
2025-02-27 20:46:38 +0200

    Update Button
    - extend it with styles for new type;

--- 02-react_ticket-generator_junior/src/components/UI/Button/Button.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/Button/Button.jsx
@@ -1,7 +1,14 @@
 import styles from "./Button.module.css";
 
-export default function Button({ className = "", children, ...rest }) {
+export const buttonStyles = {
+    util: styles["button--util"],
+    orange: styles["button--orange"]
+};
+
+export default function Button({ className = "", buttonStyle, children, ...rest }) {
     return (
-        <button className={`${className} ${styles.button}`} {...rest}>{children}</button>
+        <button className={`${buttonStyle} ${className}`} {...rest}>
+            <span>{children}</span>
+        </button>
     );
 }
