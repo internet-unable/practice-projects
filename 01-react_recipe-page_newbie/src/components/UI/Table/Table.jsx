27570ca9a9846b959d75aede80ef79dc58f7f4a2
internet-unable <k_moiseenko@icloud.com>
2025-03-01 21:11:18 +0200

    Update Table
    - change html structure from list to table;
    - add no data fallback;
    - update styles: moved values into variables, updated name to more simple;

--- 01-react_recipe-page_newbie/src/components/UI/Table/Table.jsx
+++ 01-react_recipe-page_newbie/src/components/UI/Table/Table.jsx
@@ -1,19 +1,24 @@
 import styles from "./Table.module.css";
 
 export default function Table({ data }) {
-    return (
-        <ul className={styles.table}>
-            {data.map(item => (
-                <li className={styles["table-row"]} key={item.id}>
-                    <div className={`${styles["table-col"]} ${styles["table-col--left"]}`}>
-                        <span className={styles["table-col__content"]}>{item.desc}</span>
-                    </div>
+    if (!data || data.length === 0) {
+        return <p>No data available</p>;
+    }
 
-                    <div className={`${styles["table-col"]} ${styles["table-col--right"]}`}>
-                        <span className={styles["table-col__content"]}><b>{item.value}</b></span>
-                    </div>
-                </li>
-            ))}
-        </ul>
+    return (
+        <table className={styles.table}>
+            <tbody>
+                {data.map(item => (
+                    <tr className={styles.row} key={item.id}>
+                        <td className={`${styles.col} ${styles["col--left"]}`}>
+                            <span className={styles["col-content"]}>{item.desc}</span>
+                        </td>
+                        <td className={`${styles.col} ${styles["col--right"]}`}>
+                            <span className={styles["col-content"]}><b>{item.value}</b></span>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     );
 }
