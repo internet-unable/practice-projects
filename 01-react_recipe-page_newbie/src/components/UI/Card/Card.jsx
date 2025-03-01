938c511eb6cd929cedcc4aa15b0362e964e0f899
internet-unable <k_moiseenko@icloud.com>
2025-03-01 21:18:26 +0200

    Updated Card
    - change name of configuration object and it's properties to uppercase;
    - update styles: moved values into variables;

--- 01-react_recipe-page_newbie/src/components/UI/Card/Card.jsx
+++ 01-react_recipe-page_newbie/src/components/UI/Card/Card.jsx
@@ -1,17 +1,31 @@
 import styles from "./Card.module.css";
 
-export const cardSizes = {
-    L: "L",
-    S: "S"
-}
+export const CARD_SIZES = {
+    LARGE: 'large',
+    SMALL: 'small',
+};
 
-export default function Card({ className = "", cardSize = "L", cardHeader, cardBody, ...rest }) {
+export default function Card({ 
+    className = "", 
+    cardSize = CARD_SIZES.LARGE, 
+    cardHeader, 
+    cardBody, 
+    ...rest 
+}) {
     let classes = `${className} ${styles.card} ${styles[`card--${cardSize}`]}`;
 
     return (
         <section className={classes} {...rest}>
-            {cardHeader && (<div className={styles["card-header"]}>{cardHeader}</div>)}
-            {cardBody && (<div className={styles["card-body"]}>{cardBody}</div>)}
+            {cardHeader && (
+                <div className={styles["card-header"]}>
+                    {cardHeader}
+                </div>
+            )}
+            {cardBody && (
+                <div className={styles["card-body"]}>
+                    {cardBody}
+                </div>
+            )}
         </section>
     );
 }
