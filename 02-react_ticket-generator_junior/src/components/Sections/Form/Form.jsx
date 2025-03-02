98bca463e4982bd0babb9b33467961a14baffb96
internet-unable <k_moiseenko@icloud.com>
2025-03-02 22:07:42 +0200

    Update Form
    - small naming updates;
    - comment required, unti I develop ticket page;

--- 02-react_ticket-generator_junior/src/components/Sections/Form/Form.jsx
+++ 02-react_ticket-generator_junior/src/components/Sections/Form/Form.jsx
@@ -5,20 +5,20 @@ import Button, { buttonStyles } from "../../UI/Button/Button";
 import styles from "./Form.module.css";
 
 export default function Form({ onSubmit }) {
-    function handleFormValidation(event) {
+    function handleFormSubmit(event) {
         event.preventDefault();
         onSubmit();
     }
 
     return (
-        <form onSubmit={handleFormValidation}>
+        <form onSubmit={handleFormSubmit}>
             <Upload
                 className={styles["form-item"]}
                 label="Upload Avatar"
                 name="avatar"
                 defaultMessage="Upload your photo (JPG or PNG, max size: 500KB)."
                 errorMessage="File too large. Please upload a photo under 500KB."
-                required
+                // required
             />
 
             <Input
@@ -28,7 +28,7 @@ export default function Form({ onSubmit }) {
                 name="full-name"
                 pattern="[A-Za-z]+"
                 title="Numbers and special characters are not allowed."
-                required
+                // required
             />
 
             <Input
@@ -37,9 +37,8 @@ export default function Form({ onSubmit }) {
                 type="email"
                 name="email"
                 placeholder="example@email.com"
-                // message="Please enter a valid email address."
                 title="Please enter a valid email address."
-                required
+                // required
             />
 
             <Input
@@ -49,8 +48,8 @@ export default function Form({ onSubmit }) {
                 name="github-username"
                 placeholder="@yourusername"
                 pattern="^@[A-Za-z0-9_]+$"
-                title="It should start with @ and contain only letters, numbers, and underscores."
-                required
+                title="Your username should start with @ and contain only letters, numbers, and underscores."
+                // required
             />
 
             <Button
