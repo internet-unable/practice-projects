2ffec108554c3dccb9aebe376d3a055421ae34d2
internet-unable <k_moiseenko@icloud.com>
2025-03-02 22:07:02 +0200

    Update Ticket
    - apply new components;
    - remove test styles;

--- 02-react_ticket-generator_junior/src/components/Sections/Ticket/Ticket.jsx
+++ 02-react_ticket-generator_junior/src/components/Sections/Ticket/Ticket.jsx
@@ -1,30 +1,39 @@
+import Group, { groupStyles } from "../../UI/Group/Group";
+import LogoIcon, { iconStyles } from "../../UI/Icons/Logo/Logo";
+import Heading from "../../UI/Heading/Heading";
+import Paragraph, { paragraphStyles } from "../../UI/Paragraph/Paragraph";
+import GithubIcon from "../../UI/Icons/Github/Github";
+
 import styles from "./Ticket.module.css";
 
 export default function Ticket() {
     return (
-        <div className={styles.test}>
-            <div className={styles.ticket}>
-                <div className={styles["ticket-inner"]}>
-                    <div className={styles["ticket-logo"]}>
-                        <div className={styles["logo-icon"]}>⚙️</div>
-                        <h3>Coding Conf</h3>
-                    </div>
-                    <div>Jan 31, 2025 / Austin, TX</div>
+        <div className={styles.ticket}>
+            <Group groupStyle={`${groupStyles.gap20} ${groupStyles.alignStart}`}>
+                <LogoIcon iconStyle={`${iconStyles.size40} ${iconStyles.colorDefault}`} />
+                <div>
+                    <Heading tag="h2" style={{ marginTop: "-4px", marginBottom: "19px" }}>Coding Conf</Heading>
+                    <Group groupStyle={`${groupStyles.gap15} ${groupStyles.alignCenter}`}>
+                        <Paragraph paragraphStyle={paragraphStyles.size18}>Jan 31, 2025</Paragraph>
+                        <Paragraph paragraphStyle={paragraphStyles.size18}>/</Paragraph>
+                        <Paragraph paragraphStyle={paragraphStyles.size18}>Austin, TX</Paragraph>
+                    </Group>
                 </div>
+            </Group>
 
-                <div className={styles["ticket-details"]}>
-                    <div className={styles.profile}>
-                        <div className={styles["profile-pic"]}></div>
-                        <div className={styles["profile-info"]}>
-                            <h3>Jonatan Kristof</h3>
-                            <div className={styles["profile-social"]}>
-                                <span>@jonatankristof0101</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={styles["ticket-number"]}>#80109</div>
+            <Group groupStyle={`${groupStyles.gap16} ${groupStyles.alignStart}`}>
+                <div className={styles.image}></div>
+                <div>
+                    <Heading tag="h4" style={{ marginTop: "11px", marginBottom: "10px" }}>Jonatan Kristof</Heading>
+                    <Group>
+                        <GithubIcon />
+                        <Paragraph paragraphStyle={paragraphStyles.size19}>@jonatankristof0101</Paragraph>
+                    </Group>
                 </div>
+            </Group>
+
+            <div className={styles["ticket-number"]}>
+                <Heading tag="h5">#01609</Heading>
             </div>
         </div>
     );
