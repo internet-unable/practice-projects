e10bb1589e5e8b749ab0aba6426f8cac68c5b8d0
internet-unable <k_moiseenko@icloud.com>
2025-03-02 22:03:13 +0200

    Rename InfoIcon to Info
    - updated styles with pre-sets;

--- 02-react_ticket-generator_junior/src/components/UI/Icons/Info/Info.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/Icons/Info/Info.jsx
@@ -1,11 +1,12 @@
-import styles from "./InfoIcon.module.css";
+import styles from "./Info.module.css";
 
 export const iconStyles = {
-    default: styles["icon--default"],
-    invalid: styles["icon--invalid"],
+    size16: styles["icon-size--16"],
+    colorDefault: styles["icon-color--default"],
+    colorInvalid: styles["icon-color--invalid"],
 }
 
-export default function InfoIcon({ className = "", iconStyle = iconStyles.default }) {
+export default function Info({ className = "", iconStyle = `${iconStyles.size16} ${iconStyles.colorDefault}` }) {
     return (
         <svg className={`${styles.icon} ${iconStyle} ${className}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
             <path strokeLinecap="round" strokeLinejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z" />
