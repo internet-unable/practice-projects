d600c9bf72a8cfb143e54eab629d3e8bea8e1117
internet-unable <k_moiseenko@icloud.com>
2025-03-05 20:44:25 +0200

    Update for Heading
    - add configuration object;

--- 02-react_ticket-generator_junior/src/components/UI/Heading/Heading.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/Heading/Heading.jsx
@@ -1,9 +1,29 @@
 import styles from "./Heading.module.css";
 
-export default function Heading({ tag = "h1", className = "", children, ...rest}) {
+export const headingStyles = {
+    h1: styles.h1,
+    h2: styles.h2,
+    h3: styles.h3,
+    h4: styles.h4,
+    h5: styles.h5,
+    h6: styles.h6
+}
+
+export default function Heading({
+    tag = "div",
+    headingStyle = headingStyles.h1,
+    className = "",
+    children,
+    ...rest
+}) {
     const HeadingTag = tag;
 
     return (
-        <HeadingTag className={`${styles[tag]} ${className}`} {...rest}>{children}</HeadingTag>
+        <HeadingTag
+            className={`${className} ${headingStyle}`}
+            {...rest}
+        >
+            {children}
+        </HeadingTag>
     );
 }
