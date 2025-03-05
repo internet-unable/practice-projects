0169bde39ed5892c8546dd19a4630520c55a647f
internet-unable <k_moiseenko@icloud.com>
2025-03-05 20:45:04 +0200

    Update for Ticket
    - styles cleanup;
    - apply changes

--- 02-react_ticket-generator_junior/src/components/Sections/Ticket/Ticket.jsx
+++ 02-react_ticket-generator_junior/src/components/Sections/Ticket/Ticket.jsx
@@ -1,6 +1,7 @@
-import Group, { groupStyles } from "../../UI/Group/Group";
+import Row, { rowStyles } from "../../UI/Row/Row";
+import Col, { colStyles } from "../../UI/Col/Col";
 import LogoIcon, { iconStyles } from "../../UI/Icons/Logo/Logo";
-import Heading from "../../UI/Heading/Heading";
+import Heading, { headingStyles } from "../../UI/Heading/Heading";
 import Paragraph, { paragraphStyles } from "../../UI/Paragraph/Paragraph";
 import GithubIcon from "../../UI/Icons/Github/Github";
 
@@ -9,32 +10,55 @@ import styles from "./Ticket.module.css";
 export default function Ticket() {
     return (
         <div className={styles.ticket}>
-            <Group groupStyle={`${groupStyles.gap20} ${groupStyles.alignStart}`}>
-                <LogoIcon iconStyle={`${iconStyles.size40} ${iconStyles.colorDefault}`} />
-                <div>
-                    <Heading tag="h2" style={{ marginTop: "-4px", marginBottom: "19px" }}>Coding Conf</Heading>
-                    <Group groupStyle={`${groupStyles.gap15} ${groupStyles.alignCenter}`}>
-                        <Paragraph paragraphStyle={paragraphStyles.size18}>Jan 31, 2025</Paragraph>
-                        <Paragraph paragraphStyle={paragraphStyles.size18}>/</Paragraph>
-                        <Paragraph paragraphStyle={paragraphStyles.size18}>Austin, TX</Paragraph>
-                    </Group>
-                </div>
-            </Group>
+            <Row rowStyle={`${rowStyles.justifySpaceBetween} ${rowStyles.alignCenter}`}>
+                <Col colStyle={`${colStyles.gap74}`}>
+                    <Row rowStyle={`${rowStyles.gap20} ${rowStyles.alignStart}`}>
+                        <LogoIcon iconStyle={`${iconStyles.size40} ${iconStyles.colorDefault}`} />
+                        <div>
+                            <Heading
+                                tag="h2"
+                                headingStyle={headingStyles.h2}
+                                style={{ marginTop: "-4px", marginBottom: "19px" }}
+                            >
+                                Coding Conf
+                            </Heading>
+                            <Row rowStyle={`${rowStyles.gap15} ${rowStyles.alignCenter}`}>
+                                <Paragraph paragraphStyle={paragraphStyles.size18}>Jan 31, 2025</Paragraph>
+                                <Paragraph paragraphStyle={paragraphStyles.size18}>/</Paragraph>
+                                <Paragraph paragraphStyle={paragraphStyles.size18}>Austin, TX</Paragraph>
+                            </Row>
+                        </div>
+                    </Row>
 
-            <Group groupStyle={`${groupStyles.gap16} ${groupStyles.alignStart}`}>
-                <div className={styles.image}></div>
-                <div>
-                    <Heading tag="h4" style={{ marginTop: "11px", marginBottom: "10px" }}>Jonatan Kristof</Heading>
-                    <Group>
-                        <GithubIcon />
-                        <Paragraph paragraphStyle={paragraphStyles.size19}>@jonatankristof0101</Paragraph>
-                    </Group>
-                </div>
-            </Group>
+                    <Row rowStyle={`${rowStyles.gap16} ${rowStyles.alignStart}`}>
+                        <div className={styles.image}></div>
+                        <div>
+                            <Heading
+                                tag="h4"
+                                headingStyle={headingStyles.h4}
+                                style={{ marginTop: "11px", marginBottom: "10px" }}
+                            >
+                                Jonatan Kristof
+                            </Heading>
+                            <Row>
+                                <GithubIcon />
+                                <Paragraph paragraphStyle={paragraphStyles.size19}>@jonatankristof0101</Paragraph>
+                            </Row>
+                        </div>
+                    </Row>
+                </Col>
 
-            <div className={styles["ticket-number"]}>
-                <Heading tag="h5">#01609</Heading>
-            </div>
+                <div>
+                    <Heading
+                        tag="p"
+                        headingStyle={headingStyles.h5}
+                        className={styles.number}
+                        style={{ marginTop: "-2px" }}
+                    >
+                        #01609
+                    </Heading>
+                </div>
+            </Row>
         </div>
     );
 }
