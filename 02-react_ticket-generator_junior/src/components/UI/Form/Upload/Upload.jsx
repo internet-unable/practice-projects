1f6343f20e89aed816195a3f9bb6a0b7286af5f2
internet-unable <k_moiseenko@icloud.com>
2025-03-06 23:06:58 +0200

    Update Upload
    - add drag and drop;
    - add usage of clsx, for more convinient classnames;

--- 02-react_ticket-generator_junior/src/components/UI/Form/Upload/Upload.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/Form/Upload/Upload.jsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { useRef, useState } from "react";
 
 import UploadIcon from "../../Icons/Upload/Upload";
@@ -8,12 +9,19 @@ import InfoIcon, { iconStyles } from "../../Icons/Info/Info";
 
 import styles from "./Upload.module.css";
 
-const UPLOAD_CONFIG = {
+export const UPLOAD_CONFIG = {
     // MAX_FILE_SIZE: 500 * 1024,
     MAX_FILE_SIZE: 50 * 1024,
-    ACCEPTED_TYPES: ".jpg, .jpeg, .png",
-    DEFAULT_MESSAGE: 'Upload your file',
-    DEFAULT_ERROR_MESSAGE: 'Error processing file. Please try again later.'
+    ACCEPTED_TYPES: {
+        DEFAULT: ".jpg",
+        IMAGES: ".jpg, .jpeg, .png",
+    },
+    MESSAGES: {
+        DEFAULT: 'Upload your file',
+        ERROR: 'Error processing file. Please try again later.',
+        IMAGE_DEFAULT: "Upload your photo (JPG or PNG, max size: 500KB).",
+        IMAGE_SIZE_ERROR: 'File too large. Please upload a photo under 500KB.'
+    }
 };
 
 function validateFile(file, maxSize) {
@@ -21,66 +29,95 @@ function validateFile(file, maxSize) {
     return file.size <= maxSize;
 }
 
+function readFile(file) {
+    return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+    });
+};
+
 export default function Upload({
-        className = "",
-        label = "Upload file",
-        name = "file",
-        defaultMessage = UPLOAD_CONFIG.DEFAULT_MESSAGE,
-        errorMessage = UPLOAD_CONFIG.DEFAULT_ERROR_MESSAGE,
-        accept = UPLOAD_CONFIG.ACCEPTED_TYPES,
-        ...rest
-    }) {
+    className = "",
+    label = "Upload file",
+    name = "file",
+    defaultMessage = UPLOAD_CONFIG.MESSAGES.DEFAULT,
+    errorMessage = UPLOAD_CONFIG.MESSAGES.ERROR,
+    accept = UPLOAD_CONFIG.ACCEPTED_TYPES.DEFAULT,
+    ...rest
+}) {
     const inputRef = useRef(null);
     const [uploadState, setUploadState] = useState({
         image: null,
         isValidFileSize: null,
         message: defaultMessage
     });
-    let messageStyle = messageStyles.colorDefault;
-    let messageIconStyle = `${iconStyles.size16} ${iconStyles.colorDefault}`;
+    const [isDragging, setIsDragging] = useState(false);
 
-    if (uploadState.isValidFileSize === false) {
-        messageStyle = messageStyles.colorInvalid;
-        messageIconStyle = `${iconStyles.size16} ${iconStyles.colorInvalid}`;
-    }
+    const uploadClasses = clsx({
+        [styles.upload]: true,
+        [className]: true
+    });
+    const decoratorContentClasses = clsx({
+        [styles["decorator-content"]]: true,
+        [styles["decorator-content--is-dragging"]]: isDragging
+    });
+    const decoratorContentDefaultClasses = clsx({
+        [decoratorContentClasses]: true,
+        [styles["decorator-content--default"]]: true
+    });
+    const messageStyle = clsx({
+        [messageStyles.colorDefault]: uploadState.isValidFileSize !== false,
+        [messageStyles.colorInvalid]: uploadState.isValidFileSize === false
+    });
+    const messageIconStyle = clsx({
+        [iconStyles.size16]: true,
+        [iconStyles.colorDefault]: uploadState.isValidFileSize !== false,
+        [iconStyles.colorInvalid]: uploadState.isValidFileSize === false
+    });
 
-    function handleInputChange(event) {
+    async function manageFile(file, event) {
+        // No file at all
+        if (!file) return;
+
+        // File size is invalid, too large
+        const isFileSizeValid = validateFile(file, UPLOAD_CONFIG.MAX_FILE_SIZE);
+        if (!isFileSizeValid) {
+            setUploadState(prev => ({
+                ...prev,
+                isValidFileSize: false,
+                message: errorMessage
+            }));
+            event.target.value = "";
+            return;
+        }
+
+        // File size is valid
         try {
-            // No file at all
-            const file = event.target.files[0];
-            if (!file) return;
+            const imageData = await readFile(file);
 
-            // File size is too large
-            const isFileValid = validateFile(file, UPLOAD_CONFIG.MAX_FILE_SIZE);
-            if (!isFileValid) {
-                setUploadState(prev => ({
-                    ...prev,
-                    isValidFileSize: false,
-                    message: errorMessage
-                }));
-                event.target.value = "";
-                return;
-            }
-
-            // File is valid
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setUploadState(prev => ({
-                    ...prev,
-                    image: reader.result,
-                    isValidFileSize: true,
-                    message: defaultMessage
-                }));
-            };
-            reader.readAsDataURL(file);
+            setUploadState(prev => ({
+                ...prev,
+                image: imageData,
+                isValidFileSize: true,
+                message: defaultMessage
+            }));
+            // imageData.readAsDataURL(file);
         } catch (error) {
             // Error processing file
             setUploadState(prev => ({
                 ...prev,
                 isValidFileSize: false,
-                message: UPLOAD_CONFIG.DEFAULT_ERROR_MESSAGE
+                message: UPLOAD_CONFIG.MESSAGES.ERROR
             }));
         }
+    }
+
+    function handleInputChange(event) {
+        const file = event.target.files[0];
+
+        manageFile(file, event);
     }
 
     function handleEnterKeyDown(event) {
@@ -89,7 +126,7 @@ export default function Upload({
         }
     }
 
-    function handleRemoveImage() {
+    function handleRemoveImageClick() {
         setUploadState(prev => ({
             ...prev,
             image: null,
@@ -98,17 +135,45 @@ export default function Upload({
         inputRef.current.value = "";
     }
 
-    function handleChangeImage() {
+    function handleChangeImageClick() {
         inputRef.current.click();
     }
 
+    function handleDragOver(event) {
+        event.preventDefault();
+        setIsDragging(true);
+    };
+
+    function handleDragLeave(event) {
+        event.preventDefault();
+        setIsDragging(false);
+    };
+
+    function handleDrop(event) {
+        event.preventDefault();
+        setIsDragging(false);
+        const droppedFile = event.dataTransfer.files[0];
+
+        manageFile(droppedFile, event);
+    };
+
     return (
-        <div className={`${styles.upload} ${className}`}>
+        <div className={uploadClasses}>
             {label && <div className={styles.fake__label}>{label}</div>}
 
-            <div className={styles.decorator}>
+            <div
+                className={styles.decorator}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+            >
                 {!uploadState.image && (
-                    <label className={`${styles["decorator-content"]} ${styles["decorator-content--default"]}`} htmlFor={name} tabIndex="0" onKeyUp={handleEnterKeyDown}>
+                    <label
+                        className={decoratorContentDefaultClasses}
+                        htmlFor={name}
+                        tabIndex="0"
+                        onKeyUp={handleEnterKeyDown}
+                    >
                         <div className={styles["decorator-content__icon"]}>
                             <UploadIcon />
                         </div>
@@ -116,11 +181,11 @@ export default function Upload({
                     </label>
                 )}
                 {uploadState.image && (
-                    <div className={styles["decorator-content"]}>
+                    <div className={decoratorContentClasses}>
                         <img className={styles["decorator-content__image"]} src={uploadState.image} alt="" />
-                        <Row>
-                            <Button buttonStyle={buttonStyles.util} type="button" onClick={handleRemoveImage}>Remove image</Button>
-                            <Button buttonStyle={buttonStyles.util} type="button" onClick={handleChangeImage}>Change image</Button>
+                        <Row className={styles["decorator-content__btn-row"]}>
+                            <Button buttonStyle={buttonStyles.util} type="button" onClick={handleRemoveImageClick}>Remove image</Button>
+                            <Button buttonStyle={buttonStyles.util} type="button" onClick={handleChangeImageClick}>Change image</Button>
                         </Row>
                     </div>
                 )}
