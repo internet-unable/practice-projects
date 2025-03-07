0574a37e23451bdd5c79f5b333cf656f90e6f8ee
internet-unable <k_moiseenko@icloud.com>
2025-03-07 22:04:42 +0200

    Updat Input
    - update styles namig;
    - comment code for errors;

--- 02-react_ticket-generator_junior/src/components/UI/Form/Input/Input.jsx
+++ 02-react_ticket-generator_junior/src/components/UI/Form/Input/Input.jsx
@@ -2,13 +2,13 @@ import styles from "./Input.module.css";
 
 export default function Input({ className = "", label, type = "text", name, message, isValid = false, ...rest }) {
     return (
-        <div className={`${className} ${styles["input-group"]}`}>
+        <div className={`${className} ${styles.input}`}>
             {label && <label htmlFor={name}>{label}</label>}
 
             <input type={type} name={name} id={name} {...rest} />
 
-            {message && (
-                <div className={`${styles["input-group__message"]} ${!isValid ? styles["input-group__message--invalid"] : ""}`}>
+            {/* {message && (
+                <div className={`${styles["input-group__message"]} ${!isValid ? styles["input__message--invalid"] : ""}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2 8a6 6 0 1 0 12 0A6 6 0 0 0 2 8Z" />
                         <path d="M8.004 10.462V7.596ZM8 5.57v-.042Z" />
@@ -16,7 +16,7 @@ export default function Input({ className = "", label, type = "text", name, mess
                     </svg>
                     {message}
                 </div>
-            )}
+            )} */}
         </div>
     );
 }
