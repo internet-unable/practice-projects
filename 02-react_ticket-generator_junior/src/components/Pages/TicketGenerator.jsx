d1baebadb7da1b28e7177505bcad4526313cc748
internet-unable <k_moiseenko@icloud.com>
2025-03-15 14:53:11 +0200

    Update TicketGenerator
    - apply Container;
    - separate component into two parts (Form view and Ticket view);

--- 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
+++ 02-react_ticket-generator_junior/src/components/Pages/TicketGenerator.jsx
@@ -1,6 +1,7 @@
 // import { useFormStatus } from "react-dom";
 import { useState } from "react";
 
+import Container, { containerStyles } from "../UI/Container/Container";
 import Header from "../Sections/Header/Header";
 import Heading from "../UI/Heading/Heading";
 import Paragraph, { paragraphStyles } from "../UI/Paragraph/Paragraph";
@@ -9,51 +10,76 @@ import Ticket from "../Sections/Ticket/Ticket";
 
 export default function TicketGenerator() {
     // const { pending, data, method, action } = useFormStatus();
-
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    let headingContent = <>Your Journey to Coding Conf 2025 Starts Here!</>;
-    let paragraphContent = <>Secure your spot at next year`s biggest coding conference.</>;
-
-    if (name) {
-        headingContent = <>Congrats, <span className="c-gradient--orange-white">{name}!</span><br />Your ticket is ready.</>;
-    }
-
-    if (email) {
-        paragraphContent = <>We've emailed your ticket to<br /><span className="c-orange--700">{email}</span> and will send updates in<br />the run up to the event.</>
-    }
+    const [state, setState] = useState({
+        userName: null,
+        userEmail: null,
+        isFormSubmitedSuccess: false
+    });
 
     function handleFormSubmit() {
-        setName("Jonatan Kristof");
-        setEmail("jonatan@email.com");
+        setState(prevState => {
+            return {
+                ...prevState,
+                userName: "Jonatan Kristof",
+                userEmail: "jonatan@email.com",
+                isFormSubmitedSuccess: true
+            };
+        });
     }
 
     return (
         <>
             <Header />
 
-            <main style={{ paddingTop: name ? "70px" : "60px", paddingBottom: "140px" }}>
-                <section className="text--center" style={{ marginBottom: name ? "110px" : "48px" }}>
-                    <Heading tag="h1" style={{ marginBottom: name && "33px" }}>{headingContent}</Heading>
-                    <Paragraph paragraphStyle={paragraphStyles.size23}>{paragraphContent}</Paragraph>
-                </section>
+            <main style={{ marginBottom: "120px" }}>
+                {!state.isFormSubmitedSuccess && (
+                    <>
+                        <Container className="text--center" containerStyle={containerStyles.maxWidth800}>
+                            <Heading
+                                tag="h1"
+                                style={{ marginBottom: "33px" }}
+                            >
+                                Your Journey to Coding Conf 2025 Starts Here!
+                            </Heading>
 
-                <section>
-                    {!name && (
-                        <Form
-                            className="margin--center"
-                            onSubmit={handleFormSubmit}
-                            style={{ width: "462px" }}
-                        />
-                    )}
+                            <Paragraph
+                                paragraphStyle={paragraphStyles.size23}
+                            >
+                                Secure your spot at next year`s biggest coding conference.
+                            </Paragraph>
+                        </Container>
 
-                    {name && (
-                        <Ticket
-                            className="margin--center"
-                            style={{ width: "600px" }}
-                        />
-                    )}
-                </section>
+                        <Container containerStyle={containerStyles.maxWidth462} style={{ marginTop: "48px" }}>
+                            <Form onSubmit={handleFormSubmit} />
+                        </Container>
+                    </>
+                )}
+
+                {state.isFormSubmitedSuccess && (
+                    <>
+                        <Container className="text--center" containerStyle={containerStyles.maxWidth800}>
+                            <Heading
+                                tag="h1"
+                                style={{ marginBottom: "20px" }}
+                            >
+                                Congrats, <span className="c-gradient--orange-white">{state.userName}</span>! Your ticket is ready.
+                            </Heading>
+
+                            <Paragraph
+                                paragraphStyle={paragraphStyles.size23}
+                            >
+                                We've emailed your ticket to <span className="c-orange--700">{state.userEmail}</span> and will send updates in the run up to the event.
+                            </Paragraph>
+                        </Container>
+
+                        <Container
+                            containerStyle={containerStyles.maxWidth600}
+                            style={{ marginTop: "110px" }}
+                        >
+                            <Ticket />
+                        </Container>
+                    </>
+                )}
             </main>
         </>
     );
