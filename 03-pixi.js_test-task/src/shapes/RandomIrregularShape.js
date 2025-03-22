827f2d45ebce97d8424844f8e04658a8876f2415
internet-unable <k_moiseenko@icloud.com>
2025-03-22 17:57:29 +0200

    Create RandomIrregularShape

---
+++ 03-pixi.js_test-task/src/shapes/RandomIrregularShape.js
@@ -0,0 +1,77 @@
+import Shape from "./Shape";
+
+const SIZE = 40;
+const MIN_POINTS = 5;
+const MAX_POINTS = 12;
+
+export default class RandomIrregularShape extends Shape {
+    constructor(x, y, size = SIZE, color) {
+        super(x, y + size / 2, color);
+        this.size = size;
+        this.points = [];
+        this.generatePoints();
+        this.draw();
+    }
+
+    generatePoints() {
+        const numPoints =
+            Math.floor(Math.random() * (MAX_POINTS - MIN_POINTS + 1)) +
+            MIN_POINTS;
+
+        for (let i = 0; i < numPoints; i++) {
+            const baseAngle = (i * 2 * Math.PI) / numPoints;
+
+            const angleVariation =
+                (Math.random() * 0.3 - 0.15) * ((2 * Math.PI) / numPoints);
+            const angle = baseAngle + angleVariation;
+
+            const radiusVariation = 0.7 + Math.random() * 0.6;
+            const radius = (this.size / 2) * radiusVariation;
+
+            const x = radius * Math.cos(angle);
+            const y = radius * Math.sin(angle);
+
+            this.points.push({ x, y });
+        }
+    }
+
+    draw() {
+        super.draw();
+
+        this.graphics.beginPath();
+
+        const firstPoint = this.points[0];
+        this.graphics.moveTo(
+            firstPoint.x * this.scale,
+            firstPoint.y * this.scale
+        );
+
+        for (let i = 1; i < this.points.length; i++) {
+            const point = this.points[i];
+            this.graphics.lineTo(point.x * this.scale, point.y * this.scale);
+        }
+
+        this.graphics.closePath();
+        this.graphics.fill();
+        this.graphics.x = this.x;
+        this.graphics.y = this.y;
+    }
+
+    // resize(scaleFactor) {
+    //     super.resize(scaleFactor);
+    // }
+
+    getArea() {
+        let area = 0;
+        const numPoints = this.points.length;
+
+        for (let i = 0; i < numPoints; i++) {
+            const j = (i + 1) % numPoints;
+            area += this.points[i].x * this.points[j].y;
+            area -= this.points[j].x * this.points[i].y;
+        }
+
+        area = (Math.abs(area) / 2) * this.scale * this.scale;
+        return area;
+    }
+}
