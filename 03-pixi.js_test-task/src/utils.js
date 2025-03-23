d150fb58fd82c94e0d4b455828d14d40fe39af1e
internet-unable <k_moiseenko@icloud.com>
2025-03-23 23:38:51 +0200

    Delete utils.js

--- 03-pixi.js_test-task/src/utils.js
+++
@@ -1,6 +0,0 @@
-export function getAbsoluteHeight(el) {
-    const styles = window.getComputedStyle(el);
-    const margin = parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
-
-    return Math.ceil(el.offsetHeight + margin);
-}
