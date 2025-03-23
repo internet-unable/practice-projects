7591b5b9b89794c4fe0adc326f26c070cb9ae3c4
internet-unable <k_moiseenko@icloud.com>
2025-03-23 15:27:46 +0200

    Create utils.js

---
+++ 03-pixi.js_test-task/src/utils.js
@@ -0,0 +1,6 @@
+export function getAbsoluteHeight(el) {
+    const styles = window.getComputedStyle(el);
+    const margin = parseFloat(styles["marginTop"]) + parseFloat(styles["marginBottom"]);
+
+    return Math.ceil(el.offsetHeight + margin);
+}
