29ef191dee96631c85aa375c9e4c9f1cd2b7f237
internet-unable <k_moiseenko@icloud.com>
2025-03-25 18:38:25 +0200

    Update Divider
    - switch from div to hr tag;

--- 01-react_recipe-page_newbie/src/components/UI/Divider/Divider.jsx
+++ 01-react_recipe-page_newbie/src/components/UI/Divider/Divider.jsx
@@ -2,6 +2,6 @@ import styles from "./Divider.module.css";
 
 export default function Divider({ className = "", ...rest }) {
     return (
-        <div className={`${className} ${styles.divider}`} {...rest}></div>
+        <hr className={`${className} ${styles.divider}`} {...rest} />
     );
 }
