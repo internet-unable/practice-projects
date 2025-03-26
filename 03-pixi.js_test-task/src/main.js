e0a235ca636ec44bcc66c9e5b1a96d956ef56576
internet-unable <k_moiseenko@icloud.com>
2025-03-26 18:42:23 +0200

    Update main
    - remove connection between Model and View;
    - add some comments for resize listener;

--- 03-pixi.js_test-task/src/main.js
+++ 03-pixi.js_test-task/src/main.js
@@ -8,44 +8,59 @@ import GameController from "./core/GameController";
 
 import "./style.css";
 
-const entry = document.getElementById("app");
-entry.style.padding = `${BASE_SETTINGS.ENTRY_POINT_PADDING}px`;
-let gameApp, gameModel, gameView, gameController;
+const entryPoint = document.getElementById("app");
+let gameBoard, gameModel, gameView, gameController;
 let dimensions = calculateDimensions();
 
 (async () => {
-    gameApp = new Application();
+    gameBoard = new Application();
 
-    await gameApp.init({
+    await gameBoard.init({
         width: dimensions.CANVAS_WIDTH,
         height: dimensions.CANVAS_HEIGHT,
         background: BASE_SETTINGS.CANVAS_BG_COLOR,
     });
-    entry.insertBefore(gameApp.canvas, BASE_SETTINGS.CTRL_GROUP);
+    entryPoint.insertBefore(gameBoard.canvas, BASE_SETTINGS.CTRL_GROUP);
+    entryPoint.style.padding = `${BASE_SETTINGS.ENTRY_POINT_PADDING}px`;
 
     gameModel = new GameModel();
-    gameView = new GameView(gameApp, gameModel, dimensions);
-    gameController = new GameController(gameApp, gameModel, gameView, dimensions);
+    gameView = new GameView(gameBoard, dimensions);
+    gameController = new GameController(
+        gameBoard,
+        gameModel,
+        gameView,
+        dimensions
+    );
 })();
 
 window.addEventListener("resize", () => {
+    // get prev canvas width and height
     const oldWidth = dimensions.CANVAS_WIDTH;
     const oldHeight = dimensions.CANVAS_HEIGHT;
 
+    // update dimension, according to resize
     dimensions = calculateDimensions();
 
-    const scaleX =
-        dimensions.CONTENT_WIDTH / (oldWidth - BASE_SETTINGS.STROKE_WIDTH);
-    const scaleY =
-        dimensions.CONTENT_HEIGHT /
-        (oldHeight - HEADER_SETTINGS.HEIGHT - BASE_SETTINGS.STROKE_WIDTH);
-
-    if (gameApp) {
-        gameApp.renderer.resize(dimensions.CANVAS_WIDTH, dimensions.CANVAS_HEIGHT);
+    if (gameBoard) {
+        // adjust game board size
+        gameBoard.renderer.resize(
+            dimensions.CANVAS_WIDTH,
+            dimensions.CANVAS_HEIGHT
+        );
     }
 
     if (gameView) {
+        // adjust header and content size
         gameView.resize(dimensions);
-        gameView.adjustShapesPositions(dimensions, scaleX, scaleY);
+    }
+
+    if (gameModel) {
+        const scaleX =
+            dimensions.CONTENT_WIDTH / (oldWidth - BASE_SETTINGS.STROKE_WIDTH);
+        const scaleY =
+            dimensions.CONTENT_HEIGHT /
+            (oldHeight - HEADER_SETTINGS.HEIGHT - BASE_SETTINGS.STROKE_WIDTH);
+        // adjust position for each shape (x, y)
+        gameModel.adjustShapesPositions(dimensions, scaleX, scaleY);
     }
 });
